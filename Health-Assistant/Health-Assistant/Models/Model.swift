240793762c7a2c9dce1dfa151839307be33289b9
Hoff <theloop@naver.com>
2024-11-01 15:39:37 +0900

    [feat]: 기본폴더 생성

---
+++ Health-Assistant/Health-Assistant/Models/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Health-Assistant
+//
+//  Created by Hwang_Inyoung on 11/1/24.
+//
+
+import Foundation
---
+++ Health-Assistant/Health-Assistant/ViewModels/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Health-Assistant
+//
+//  Created by Hwang_Inyoung on 11/1/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
