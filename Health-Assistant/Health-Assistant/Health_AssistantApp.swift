7c254fd9f2fa1f5c90faed6cfcb2fb28918eeba5
sueun kim <doms@kakao.com>
2024-11-01 13:39:56 +0900

    [init]: 프로젝트 생성

---
+++ Health-Assistant/Health-Assistant/Health_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_AssistantApp.swift
+//  Health-Assistant
+//
+//  Created by Soom on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Health_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTabView()
+        }
+    }
+}
---
+++ Health-Assistant/Health-Assistant/MainTabView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Health-Assistant
+//
+//  Created by Soom on 11/1/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
