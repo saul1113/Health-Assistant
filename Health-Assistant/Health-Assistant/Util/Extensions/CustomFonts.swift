e71ef8658a121df9ef0981195c072a8a47ee0d79
Soomin Kim <140694831+sumchive@users.noreply.github.com>
2024-11-03 23:37:39 +0900

    [add]: NotoSansKR 폰트 파일 추가 및 customFont 익스텐션 추가

---
+++ Health-Assistant/Health-Assistant/Util/Extensions/CustomFonts.swift
@@ -0,0 +1,48 @@
+//
+//  CustomFonts.swift
+//  Health-Assistant
+//
+//  Created by 김수민 on 11/3/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    /// Bold
+    static var bold30 = Font.custom("NotoSansKR-Bold", size: 30)
+    static var bold28 = Font.custom("NotoSansKR-Bold", size: 28)
+    static var bold24 = Font.custom("NotoSansKR-Bold", size: 24)
+    static var bold20 = Font.custom("NotoSansKR-Bold", size: 20)
+    static var bold18 = Font.custom("NotoSansKR-Bold", size: 18)
+    static var bold16 = Font.custom("NotoSansKR-Bold", size: 16)
+    
+    /// SemiBold
+    static var semibold24 = Font.custom("NotoSansKR-SemiBold", size: 24)
+    static var semibold20 = Font.custom("NotoSansKR-SemiBold", size: 20)
+    static var semibold18 = Font.custom("NotoSansKR-SemiBold", size: 18)
+    static var semibold16 = Font.custom("NotoSansKR-SemiBold", size: 16)
+    static var semibold14 = Font.custom("NotoSansKR-SemiBold", size: 14)
+    
+    /// Medium
+    static var medium24 = Font.custom("NotoSansKR-Medium", size: 24)
+    static var medium20 = Font.custom("NotoSansKR-Medium", size: 20)
+    static var medium18 = Font.custom("NotoSansKR-Medium", size: 18)
+    static var medium16 = Font.custom("NotoSansKR-Medium", size: 16)
+    static var medium14 = Font.custom("NotoSansKR-Medium", size: 14)
+    
+    
+    /// Regular
+    static var regular30 = Font.custom("NotoSansKR-Regular", size: 30)
+    static var regular28 = Font.custom("NotoSansKR-Regular", size: 28)
+    static var regular24 = Font.custom("NotoSansKR-Regular", size: 24)
+    static var regular20 = Font.custom("NotoSansKR-Regular", size: 20)
+    static var regular18 = Font.custom("NotoSansKR-Regular", size: 18)
+    static var regular16 = Font.custom("NotoSansKR-Regular", size: 16)
+    static var regular14 = Font.custom("NotoSansKR-Regular", size: 14)
+    static var regular12 = Font.custom("NotoSansKR-Regular", size: 12)
+    static var regular10 = Font.custom("NotoSansKR-Regular", size: 10)
+}
+
+//        Text("폰트사용예시")
+//            .font(.bold30) // 이렇게 사용하면 됩니다!
