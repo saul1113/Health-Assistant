1e8915051115c6b8c8ca26d30635c8bc2c14c2b7
Hoff <theloop@naver.com>
2024-11-05 15:13:44 +0900

    [feat]: maintabview 추가

--- Health-Assistant/Health-Assistant/MainTabView.swift
+++ Health-Assistant/Health-Assistant/MainTabView.swift
@@ -10,6 +10,29 @@ import SwiftUI
 struct MainTabView: View {
     var body: some View {
         TabView {
+            HomeView()
+                .tabItem() {
+                    Label("Home", systemImage: "house")
+                }
+                .tag(0)
+            
+            MedisonMainView()
+                .tabItem() {
+                    Label("medison", systemImage: "pill.fill")
+                }
+                .tag(1)
+            
+            HealthListView()
+                .tabItem() {
+                    Label("HealthList", systemImage: "list.clipboard")
+                }
+                .tag(2)
+            
+            ProFileView()
+                .tabItem() {
+                    Label("Profile", systemImage: "person.crop.circle")
+                }
+                .tag(3)
             
         }
     }
