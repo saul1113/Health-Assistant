89f0de0cca1e8da6bad503c7aeec2d8213ebe548
saul1113 <clclshtus@gmail.com>
2024-11-06 22:58:56 +0900

    [feat]: Navigation수정 및 CustomGreen수정

--- Health-Assistant/Health-Assistant/Views/LoginViews/LoginView.swift
+++ Health-Assistant/Health-Assistant/Views/LoginViews/LoginView.swift
@@ -8,118 +8,130 @@
 import SwiftUI
 
 struct LoginView: View {
+    @Environment(\.dismiss) var dismiss
     @State private var showJoinView = false
     @State private var showEmailLoginView = false
     
     var body: some View {
-        VStack {
+        NavigationStack {
             VStack {
-                Image("HAHALogo")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                HStack {
-                    Text("HAHA")
-                        .foregroundStyle(Color(uiColor: .systemGreen))
-                        .font(Font.bold24)
-                    
-                    + Text("와 함께")
-                        .font(Font.bold20)
-                }
-                
-                HStack(spacing: 0) {
-                    Text("건강 관리")
-                        .font(Font.bold20)
-                        .foregroundColor(.black)
-                        .background(Color(uiColor: .systemGreen).opacity(0.3))
-                        .cornerRadius(5)
-                    
-                    Text("를 시작하세요!")
-                        .font(Font.bold20)
-                }
-                
-                Text("검사지 검색부터 스케줄관리 까지")
-                    .foregroundStyle(Color(uiColor: .systemGray))
-                    .font(Font.semibold14)
-            }
-            
-            VStack {
-                Text ("소셜 계정으로 로그인하기")
-                    .font(Font.semibold14)
-                
-                HStack {
-                    Button {
-                        
-                    } label: {
-                        Image("GoogleLogo")
-                            .resizable()
-                            .clipShape(Circle())
-                            .frame(width: 50, height: 50)
-                        //                            .overlay(Circle().stroke(Color.black))
-                    }
-                    
-                    Button {
-                        
-                    } label: {
-                        Image("AppleLogo")
-                            .resizable()
-                            .clipShape(Circle())
-                            .frame(width: 50, height: 50)
-                    }
-                    .padding(.leading, 20)
-                    
-                }
-                .padding()
-            }
-            .padding(.top, 100)
-            
-            VStack {
-                Button(action: {
-                    showEmailLoginView = true
-                }) {
+                VStack {
+                    Image("HAHALogo")
+                        .resizable()
+                        .frame(width: 100, height: 100)
                     HStack {
-                        Image(systemName: "envelope.fill")
-                            .foregroundColor(.gray)
-                        Text("이메일로 로그인")
-                            .foregroundColor(.gray)
-                            .font(Font.semibold14)
+                        Text("HAHA")
+                            .foregroundStyle(Color.customGreen)
+                            .font(Font.bold24)
+                        
+                        + Text("와 함께")
+                            .font(Font.bold20)
                     }
-                    .frame(width: 250)
-                    .padding()
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.gray, lineWidth: 0.5)
-                    )
+                    HStack(spacing: 0) {
+                        Text("건강 관리")
+                            .font(Font.bold20)
+                            .foregroundColor(.black)
+                            .background(Color.customGreen.opacity(0.3))
+                            .cornerRadius(5)
+                        
+                        Text("를 시작하세요!")
+                            .font(Font.bold20)
+                    }
+                    
+                    Text("검사지 검색부터 스케줄관리 까지")
+                        .foregroundStyle(Color(uiColor: .systemGray))
+                        .font(Font.semibold14)
                 }
                 
-                HStack(spacing: 20) {
+                VStack {
+                    Text ("소셜 계정으로 로그인하기")
+                        .font(Font.semibold14)
+                    
+                    HStack {
+                        Button {
+                            
+                        } label: {
+                            Image("GoogleLogo")
+                                .resizable()
+                                .clipShape(Circle())
+                                .frame(width: 50, height: 50)
+                            //                            .overlay(Circle().stroke(Color.black))
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image("AppleLogo")
+                                .resizable()
+                                .clipShape(Circle())
+                                .frame(width: 50, height: 50)
+                        }
+                        .padding(.leading, 20)
+                    }
+                    .padding()
+                }
+                .padding(.top, 100)
+                
+                VStack {
                     Button(action: {
-                        showJoinView = true
+                        showEmailLoginView = true
                     }) {
-                        Text("회원가입")
+                        HStack {
+                            Image(systemName: "envelope.fill")
+                                .foregroundColor(.gray)
+                            Text("이메일로 로그인")
+                                .foregroundColor(.gray)
+                                .font(.system(size: 14, weight: .semibold))
+                        }
+                        .frame(width: 250)
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray, lineWidth: 0.5)
+                        )
                     }
-                    .font(Font.semibold14)
                     
-                    Text(" | ")
-                        .font(.caption)
-                    
-                    Button("문의하기") {
-                        // 문의하기 액션
+                    HStack(spacing: 20) {
+                        Button(action: {
+                            showJoinView = true
+                        }) {
+                            Text("회원가입")
+                                .font(.system(size: 14, weight: .semibold))
+                        }
+                        
+                        Text(" | ")
+                            .font(.caption)
+                        
+                        Button("문의하기") {
+                            // 문의하기 액션
+                        }
+                        .font(Font.semibold14)
                     }
-                    .font(Font.semibold14)
+                    .padding()
                 }
                 .padding()
             }
-            .padding()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        BackButton()
+                    }
+                }
+            }
+            .navigationBarBackButtonHidden(true)
+            .navigationDestination(isPresented: $showEmailLoginView) {
+                EmailLoginView()
+                    .navigationBarBackButtonHidden(true)
+            }
+            .navigationDestination(isPresented: $showJoinView) {
+                JoinView()
+                    .navigationBarBackButtonHidden(true)
+            }
         }
-        .fullScreenCover(isPresented: $showJoinView) {
-            JoinView() // JoinView를 전체 화면에 표시
-        }
-        .fullScreenCover(isPresented: $showEmailLoginView) {
-            EmailLoginView() // EmailLoginView를 전체 화면에 표시
-        }
-        .navigationBarBackButtonHidden(true) // 네비게이션 백 버튼 숨김
     }
 }
+
 
 #Preview {
     LoginView()
