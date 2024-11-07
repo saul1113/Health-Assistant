27945d2868276c3da75c3478182b90606d878b60
sueun kim <doms@kakao.com>
2024-11-07 23:23:52 +0900

    [feat]: CustomTabView 구현

--- Health-Assistant/Health-Assistant/Util/Extensions/CustomColors.swift
+++ Health-Assistant/Health-Assistant/Util/Extensions/CustomColors.swift
@@ -11,4 +11,5 @@ extension Color {
     static let CustomGreen = Color("CustomGreen")
     static let CustomGreen02 = Color("CustomGreen02")
     static let customGreen30 = Color.CustomGreen.opacity(0.3)
+    static let customTabColor = Color("CustomTabColor")
 }
