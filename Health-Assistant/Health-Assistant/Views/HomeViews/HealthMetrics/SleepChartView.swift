6f513028d5f001fafd98b0a7c8366903004b501b
Hoff <theloop@naver.com>
2024-11-12 16:47:21 +0900

    [style]: 커스텀폰트 적용

--- Health-Assistant/Health-Assistant/Views/HomeViews/HealthMetrics/SleepChartView.swift
+++ Health-Assistant/Health-Assistant/Views/HomeViews/HealthMetrics/SleepChartView.swift
@@ -12,88 +12,89 @@ struct SleepChartView: View {
     @StateObject private var viewModel = SleepDataViewModel()
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
-                // 수면 시간과 날짜 표시
-                if let firstSleep = viewModel.sleepData.first {
-                    let totalSleepDuration = calculateTotalSleepDuration()
-                    
-                    Text("수면 시간")
-                        .font(.headline)
-                        .padding(.horizontal)
-                    
-                    Text("\(formatDuration(totalSleepDuration))")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .padding(.horizontal)
-                        .padding(.bottom, 2)
-                    
-                    Text(formatDateToYearMonthDay(firstSleep.startDate))
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                        .padding(.horizontal)
-                        .padding(.bottom, 10)
-                }
-                
-                Text("수면 데이터")
-                    .font(.title2)
-                    .padding(.horizontal)
-                
-                Chart {
-                    ForEach(viewModel.sleepData.indices, id: \.self) { index in
-                        let stage = viewModel.sleepData[index]
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    // 수면 시간과 날짜 표시
+                    if let firstSleep = viewModel.sleepData.first {
+                        let totalSleepDuration = calculateTotalSleepDuration()
                         
-                        // 수면 단계의 색상을 stage.color로 설정
-                        RectangleMark(
-                            xStart: .value("Start Time", stage.startDate),
-                            xEnd: .value("End Time", stage.endDate),
-                            y: .value("Stage", stage.stage.rawValue)
-                        )
-                        .foregroundStyle(stage.stage.color)
+                        Text("수면 시간")
+                            .font(.semibold20)
+                            .padding(.horizontal)
                         
-                        // 다음 단계와 현재 단계의 끝을 선으로 연결
-                        if index < viewModel.sleepData.count - 1 {
-                            let nextStage = viewModel.sleepData[index + 1]
+                        Text("\(formatDuration(totalSleepDuration))")
+                            .font(.bold30)
+                            .padding(.horizontal)
+                            .padding(.bottom, 2)
+                        
+                        Text(formatDateToYearMonthDay(firstSleep.startDate))
+                            .font(.regular16)
+                            .foregroundColor(.gray)
+                            .padding(.horizontal)
+                            .padding(.bottom, 10)
+                    }
+                    
+                    Chart {
+                        ForEach(viewModel.sleepData.indices, id: \.self) { index in
+                            let stage = viewModel.sleepData[index]
                             
-                            // 선으로 연결하는 LineMark 추가
-                            LineMark(
-                                x: .value("End Time", stage.endDate),
-                                y: .value("Current Stage", stage.stage.rawValue)
+                            // 수면 단계의 색상을 stage.color로 설정
+                            RectangleMark(
+                                xStart: .value("Start Time", stage.startDate),
+                                xEnd: .value("End Time", stage.endDate),
+                                y: .value("Stage", stage.stage.rawValue)
                             )
-                            .foregroundStyle(Color.gray)
+                            .foregroundStyle(stage.stage.color)
                             
-                            LineMark(
-                                x: .value("Start Time", nextStage.startDate),
-                                y: .value("Next Stage", nextStage.stage.rawValue)
-                            )
-                            .foregroundStyle(Color.gray)
+                            // 다음 단계와 현재 단계의 끝을 선으로 연결
+                            if index < viewModel.sleepData.count - 1 {
+                                let nextStage = viewModel.sleepData[index + 1]
+                                
+                                // 선으로 연결하는 LineMark 추가
+                                LineMark(
+                                    x: .value("End Time", stage.endDate),
+                                    y: .value("Current Stage", stage.stage.rawValue)
+                                )
+                                .foregroundStyle(Color.gray)
+                                
+                                LineMark(
+                                    x: .value("Start Time", nextStage.startDate),
+                                    y: .value("Next Stage", nextStage.stage.rawValue)
+                                )
+                                .foregroundStyle(Color.gray)
+                            }
                         }
                     }
-                }
-                .chartYAxis {
-                    AxisMarks {
-                        AxisValueLabel()
+                    .chartYAxis {
+                        AxisMarks {
+                            AxisValueLabel()
+                        }
                     }
+                    .chartLegend(.hidden)
+                    .frame(height: 300)
+                    .padding()
                 }
-                .chartLegend(.hidden)
-                .frame(height: 300)
-                .padding()
-            }
-            
-            // 수면 데이터를 VStack과 ForEach로 표시
-            ForEach(viewModel.sleepData) { stage in
-                VStack(alignment: .leading, spacing: 5) {
-                    Text("수면 단계: \(stage.stage.rawValue)")
-                        .font(.headline)
-                    Text("시작 시간: \(formatDate(stage.startDate))")
-                    Text("종료 시간: \(formatDate(stage.endDate))")
+                
+                // 수면 데이터를 VStack과 ForEach로 표시
+                ForEach(viewModel.sleepData) { stage in
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("수면 단계: \(stage.stage.rawValue)")
+                            .font(.semibold18)
+                        Text("시작 시간: \(formatDate(stage.startDate))")
+                            .font(.regular16)
+                        Text("종료 시간: \(formatDate(stage.endDate))")
+                            .font(.regular16)
+                    }
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .shadow(radius: 1)
+                    .padding(.horizontal)
                 }
-                .padding()
-                .background(Color.white)
-                .cornerRadius(10)
-                .shadow(radius: 1)
-                .padding(.horizontal)
             }
+            .navigationTitle("수면 데이터")
+            .font(.bold20) // 네비게이션 타이틀 폰트 설정
         }
     }
     
